0fe1a2620d7e8ce08d73a01faecbc513675f323d
FrancaScelzi <fscelzi@udesa.edu.ar>
2021-09-19 19:22:28 -0300

    header y footer como function()

--- proyecto/src/components/Footer/Footer.js
+++ proyecto/src/components/Footer/Footer.js
@@ -1,19 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import './footer.css'
 
-export default class Footer extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        }
-    }
-
-    render(){
+export default function Footer (){
         return(
             <div className= 'footer'>
                 <p className='info'>Florencia Villaverde | Franca Scelzi | Valentina Iglesias Vinyolas</p>
                 <p className= 'info'>Copyright Kessey 2021. Todos los derechos reservados ©</p>
             </div>
         )
-    }
+    
 }
--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -1,18 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import './Header.css'
 
-export default class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        }
-    }
-
-    render(){
+export default function Header() {
         return(
             <div className= 'header'>
                 <img className='logo' src={"../images/Logo2.jpg"} />
             </div>
         )
-    }
+    
 }
